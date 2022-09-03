5da9968b721f996cf4f90fe116e2181cee1d7a02
ws070815 <111626280+ws070815@users.noreply.github.com>
2022-09-03 14:49:09 +0900

    Create Divisor.cpp

---
+++ Divisor.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void Divisor(int n) {
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			printf("%d ", i);
+		}
+	}
+}
+int main() {
+	int n; scanf("%d",&n);
+	Divisor(n);
+	return 0;
+}
